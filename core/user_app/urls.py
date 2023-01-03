2c55de026d3d6edc3950285c2ce16d0dd330aa4b
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-01-03 13:25:21 -0300

    feat: adicionando rotas #6

--- core/user_app/urls.py
+++ core/user_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from user_app import views
 from django.urls import reverse_lazy
 from django.contrib.auth import views as auth_views
-
+from user_app.profile_views import views_profile
 app_name = "user"
 
 urlpatterns = [
@@ -15,4 +15,6 @@ urlpatterns = [
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="pages/password/password_reset_done.html"), name='password_reset_done'),
     path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name="pages/password/password_reset_form.html", success_url = reverse_lazy('user:password_reset_complete')), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name="pages/password/password_reset_complete.html"), name='password_reset_complete'),
+    #####urls profiles##############
+    path('profile/', views_profile.profile_home, name="profile")
 ]

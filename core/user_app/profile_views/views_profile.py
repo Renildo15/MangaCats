f9563fef4d2fc7e0c3c9b0322476a0b07301f88e
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-01-04 15:50:17 -0300

    feat: permissions genre #2

--- core/user_app/profile_views/views_profile.py
+++ core/user_app/profile_views/views_profile.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-
-
+@login_required(login_url='user:login')
 def profile_home(request):
     return render(request, "pages/profile.html")

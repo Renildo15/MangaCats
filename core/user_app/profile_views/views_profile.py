78f4818328b976f49bd174e3d623c330118890b0
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-01-23 15:58:21 -0300

    feat: craindo pagina account #4

--- core/user_app/profile_views/views_profile.py
+++ core/user_app/profile_views/views_profile.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+
 @login_required(login_url='user:login')
 def profile_home(request):
     return render(request, "pages/profile.html")
+
+def account(request):
+    return render(request, "pages/profile/profile_page.html")
--- core/user_app/urls.py
+++ core/user_app/urls.py
@@ -10,11 +10,12 @@ urlpatterns = [
     path("login/", views.login_user, name="login"),
     path("logout/", views.logout_user, name="logout"),
     path("change_password/", views.change_password, name="change_pass"),
-     path('change_password_success/', views.change_password_success, name='change_password_success'),
+    path('change_password_success/', views.change_password_success, name='change_password_success'),
     path("password_reset/", views.password_reset_request, name="password_reset"),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="pages/password/password_reset_done.html"), name='password_reset_done'),
     path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name="pages/password/password_reset_form.html", success_url = reverse_lazy('user:password_reset_complete')), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name="pages/password/password_reset_complete.html"), name='password_reset_complete'),
     #####urls profiles##############
-    path('profile/', views_profile.profile_home, name="profile")
+    path('profile/', views_profile.profile_home, name="profile"),
+    path('account/',views_profile.account, name="account" )
 ]

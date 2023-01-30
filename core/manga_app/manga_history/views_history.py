f757efd246ef5690dfdb146f0274ca1bc43fbef4
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-01-30 15:01:44 -0300

    fix: corrigindo problema de user #40

--- core/manga_app/manga_history/views_history.py
+++ core/manga_app/manga_history/views_history.py
@@ -2,17 +2,20 @@ from ..models import HistoryManga
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required, permission_required
 
 
 def manga_history(manga, user):
 
-    if not(HistoryManga.objects.filter(manga=manga, user=user).exists()):
-        history = HistoryManga.objects.create(manga=manga, user=user)
-        history.save()
+    if user.is_authenticated:
+        if not(HistoryManga.objects.filter(manga=manga, user=user).exists()):
+            history = HistoryManga.objects.create(manga=manga, user=user)
+            history.save()
 
-        return history
+            return history
 
-
+@login_required(login_url='user:login')
+@permission_required("manga_app.view_historymanga", login_url='user:login')
 def manga_history_list(request):
     history = HistoryManga.objects.filter(user=request.user).order_by('-view_date')
 
@@ -22,6 +25,9 @@ def manga_history_list(request):
 
     return render(request, "pages/history/manga_history.html", context)
 
+
+@login_required(login_url='user:login')
+@permission_required("manga_app.delete_historymanga", login_url='user:login')
 def manga_history_reset(request):
     history = HistoryManga.objects.all()
     history.delete()
